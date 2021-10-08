caf4c3e8104a903ada48ebc48bd74478d0574855
James Capadocia <james.capadocia@gmail.com>
2021-10-08 18:30:59 -0500

    Created uuid.js as a separate file with a function to be exported to server.js when called. New notes added now are assigned a unique ID based on this exported function.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const database = require('./db/db.json')
+const uuid = require('./helper/uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -52,7 +53,7 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        // note_id: uuid(),
+        note_id: uuid(),
       };
   
       readAndAppend(newNote, './db/db.json');
@@ -62,9 +63,6 @@ app.post('/api/notes', (req, res) => {
       res.error('Error adding Note :(');
     }
   });
-
-
-
 
 
 
