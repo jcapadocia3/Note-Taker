78c7e4bd9aee82ef9c2adb20fa6c5448b7093457
James Capadocia <james.capadocia@gmail.com>
2021-10-08 13:34:15 -0500

    Created '/' root to be index.html and a path for '/notes' to redirect user to notes.html.

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
 
 
 
