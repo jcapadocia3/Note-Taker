089ba0e1a7d3a8eb676a58944052418e6e0e6e2d
James Capadocia <james.capadocia@gmail.com>
2021-10-10 21:06:58 -0500

    Commented server.js file to help a user understand the flow of code and it's functionality.

--- helper/uuid.js
+++ helper/uuid.js
@@ -1,4 +1,4 @@
+// Function to generate a random number as a unique ID
+// Exports to server.js file
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x100)
-    // .toString(16)
-    // .substring(1);
--- server.js
+++ server.js
@@ -2,30 +2,38 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const database = require('./db/db.json')
+// Helper method for generating a unique ID
 const uuid = require('./helper/uuid');
 
+// Uses all express package prewritten functionality and designates a PORT to run the app on
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Serves up all static files in directory "public"
 app.use(express.static('public'));
+
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
+// GET route for homepage
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// GET route for notes page
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
+// Function to write data to the JSON file
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
+// Function to read data from and to append/write content to it
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
@@ -38,22 +46,27 @@ const readAndAppend = (content, file) => {
     });
   };
 
-
+// GET route to retrieve all stored notes
 app.get('/api/notes', (req, res) => {
  res.json(database);
 });
 
+// POST route to add new notes to store notes
 app.post('/api/notes', (req, res) => {
   
+    // Destructuring assigment for items in req.body
     const { title, text } = req.body;
   
+    // All required properties must be present in new note before it's added
     if (req.body) {
       const newNote = {
         title,
         text,
+        // Imports function from uuid.js to assign a unique ID to new note
         note_id: uuid(),
       };
   
+      // Reads new note information and appends/writes it to db.json
       readAndAppend(newNote, './db/db.json');
       res.json(`Note added!`);
       database.push(newNote)
@@ -66,5 +79,5 @@ app.post('/api/notes', (req, res) => {
 
 
 
-
+// LISTENS to ensure app is active on designated PORT
 app.listen(PORT, () => console.log(`Listening to server on port ${PORT}`))
